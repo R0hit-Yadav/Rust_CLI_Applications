6253f8be975d9519e6e0f9e618ee690173327a5c
ROHIT YADAV <rohitkyadav2312@gmail.com>
2025-01-10 13:46:53 +0530

    CLI

--- all_applications/src/main.rs
+++ all_applications/src/main.rs
@@ -1,4 +1,19 @@
 mod to_do_list;
+
+use std::io;
 fn main() {
-    to_do_list::main();
+
+    println!("Chose one of CLI applications");
+    println!("1.TO-DO LIST MANAGER");
+
+
+    let mut input=String::new();
+    io::stdin().read_line(&mut input).expect("Enter vaild input");
+
+    match input.trim() 
+    {                                                       
+        "1" => {to_do_list::main();}
+        _=>{println!("Enter valid input");}
+        
+    }
 }
